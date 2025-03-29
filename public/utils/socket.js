342b67e6aca269fa342d73345f68f9d41cee43c5
mohakhalil <mohamed.khalil2196@gmail.com>
2025-03-29 16:23:44 +0300

    S

--- public/utils/socket.js
+++ public/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from "/socket.io/socket.io.esm.min.js";
 // Update the socket URL to your Render app
-const socket = io("https://bomberman.onrender.com");
+const socket = io("https://bomberman-bw6n.onrender.com");
 
 export default socket;
